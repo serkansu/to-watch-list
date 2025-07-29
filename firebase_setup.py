81bc80bb9bee864dac7fcc6c8b6622a8f590dd93
serkansu <suluns.tr@gmail.com>
2025-07-29 17:46:45 +0300

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,21 +1,25 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials, db
-from dotenv import load_dotenv
-
-load_dotenv()
+import base64
 
 def get_database():
-    json_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-    
-    if not json_path or not os.path.exists(json_path):
-        raise ValueError("❌ GOOGLE_APPLICATION_CREDENTIALS tanımlı değil veya dosya bulunamadı.")
+    encoded_json = os.environ.get("FIREBASE_CREDENTIALS")
 
-    cred = credentials.Certificate(json_path)
+    if not encoded_json:
+        raise ValueError("❌ FIREBASE_CREDENTIALS ortam değişkeni eksik.")
+
+    try:
+        decoded_json = base64.b64decode(encoded_json).decode("utf-8")
+        cred_dict = json.loads(decoded_json)
+    except Exception as e:
+        raise ValueError(f"❌ Firebase kimlik bilgileri decode edilemedi: {e}")
 
     if not firebase_admin._apps:
+        cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred, {
             "databaseURL": "https://serkans-to-watch-list-default-rtdb.europe-west1.firebasedatabase.app"
         })
 
-    return db.reference()
+    return db.reference("/")
